23c0599503723ab3942e6bc421982dad31ed7a1b
Perapath Kanthong <masterperapath@hotmail.com>
2018-10-15 15:58:28 +0700

    #Commit_cp002&&cp003&&cp005

--- src/app/pages/cp002/cp002.component.ts
+++ src/app/pages/cp002/cp002.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+// import { NbButtonComponent } from "@nebular/theme/components/button/button.component";
+
+var objToday = new Date(),
+  weekday = new Array('อาทิตย์','จันทร์','อังคาร','พุธ','พฤหัสบดี','ศุกร์','เสาร์'),
+  dayOfWeek = weekday[objToday.getDay()],
+  //domEnder = new Array( 'th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th' ),
+  dayOfMonth = objToday.getDate(),
+  months = new Array('มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน', 'กรกฎาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม'),
+  curMonth = months[objToday.getMonth()],
+  curYear = objToday.getFullYear()+543,
+  curHour = objToday.getHours() /*> 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours())*/,
+  curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes();
+  //curSeconds = objToday.getSeconds() < 10 ? "0" + objToday.getSeconds() : objToday.getSeconds(),
+  //curMeridiem = objToday.getHours() > 12 ? "PM" : "AM";
+var today = "วัน"+dayOfWeek+" ที่ "+dayOfMonth+" "  + " " + curMonth + " " + curYear+ "  เวลา " + curHour + ":" + curMinute;
 
 @Component({
   selector: 'ngx-cp002',
   templateUrl: './cp002.component.html',
-  styleUrls: ['./cp002.component.scss']
+  styleUrls: ['./cp002.component.scss'],
 })
 export class Cp002Component {
   title = 'MasterPerapathKanthongCp002Component';
+  headerText = 'เลือกประเภทที่จอดรถสำหรับคนจริง';
+  // displayDate = new Date().toLocaleDateString();
+  displayDate = today;
 }
+
+
+// //NbButtonComponent
+// @NbButtonComponent({
+//   selector: 'nb-button',
+// })
+// export class nbButton {
+// }
+
