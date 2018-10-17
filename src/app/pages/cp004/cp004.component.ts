92657a15aa9beb6df45f8de78e0a3dd8cff14c63
DESKTOP-G5RHD0M\MasterP'z <masterperapath@hotmail.com>
2018-10-18 04:13:44 +0700

    #router_Pages_to_Page

--- src/app/pages/cp004/cp004.component.ts
+++ src/app/pages/cp004/cp004.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ngx-cp004',
   templateUrl: './cp004.component.html',
   styleUrls: ['./cp004.component.scss']
 })
-export class Cp004Component {
+export class Cp004Component implements OnInit{
   title = 'MasterPerapathKanthongCp004Component';
+
+  constructor(private router: Router) {
+
+  }
+  ngOnInit() {
+
+  }
+
+  payment() {
+    this.router.navigateByUrl("/pages/cp005");
+  }
+
 }
--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -4,70 +4,76 @@ export const MENU_ITEMS: NbMenuItem[] = [
   {
     title: 'Landing',
     //icon: 'nb-home',
-    link: '../landingPage/landing',
+    link: '/landing',
     
   },
   {
     title: 'Dashboard',
     icon: 'nb-home',
-    link: '/pages/iot-dashboard',
+    link: '/pages/dashboard',
     home: true,
+  },
+  {
+    title: 'Profile',
+    icon: 'nb-person',
+    // link: '/pages/iot-dashboard',
   },
   {
     title: 'Menu',
     group: true,
   },
   {
-    title: 'Menu1',
+    title: 'Menu หลัก'
+    ,
     icon: 'nb-keypad',
     link: '/pages/ui-features',
     children: [
+      // {
+      //   title: 'cp001',
+      //   link: '/pages/cp001',
+      // },
       {
-        title: 'cp001',
-        link: '/pages/cp001',
-      },
-      {
-        title: 'cp002',
+        title: 'เลือกระบบการจอง',
         link: '/pages/cp002',
       },
       {
-        title: 'cp003',
+        title: 'ทำการจองด้วยระบบGeneral',
         link: '/pages/cp003',
       },
       {
-        title: 'cp004',
+        title: 'ชำระเงิน',
         link: '/pages/cp004',
       },
       {
-        title: 'cp005',
+        title: 'ออกใบเสร็จ',
         link: '/pages/cp005',
       },
-      {
-        title: 'cp006',
-        link: '/pages/cp006',
-      },
+      // {
+      //   title: 'cp006',
+      //   link: '/pages/cp006',
+      // },
     ],
   },
-  {
-    title: 'Auth',
-    icon: 'nb-locked',
-    children: [
-      {
-        title: 'Login',
-        link: '/auth/login',
-      },
-      {
-        title: 'Register',
-        link: '/auth/register',
-      },
-      {
-        title: 'Request Password',
-        link: '/auth/request-password',
-      },
-      {
-        title: 'Reset Password',
-        link: '/auth/reset-password',
-      },
-    ],
-  },
+  // {
+  //   title: 'Auth',
+  //   icon: 'nb-locked',
+  //   children: [
+  //     {
+  //       title: 'Login',
+  //       link: '/auth/login',
+  //     },
+  //     {
+  //       title: 'Register',
+  //       link: '/auth/register',
+  //     },
+  //     {
+  //       title: 'Request Password',
+  //       link: '/auth/request-password',
+  //     },
+  //     {
+  //       title: 'Reset Password',
+  //       link: '/auth/reset-password',
+  //     },
+  //   ],
+  // },
 ];
